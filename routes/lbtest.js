f7e3413b8ff3c250114d827ff40ed148cfb73aeb
Alexander Josef <alexander.josef@netcetera.com>
2021-08-29 21:17:56 +0200

    enable users, lbtest routes

--- routes/lbtest.js
+++ routes/lbtest.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   console.log("lbtest router ---")
 
-  res.send('respond with a resource -- req IP : ' + req.ip);
+  let resBody = `respond with a resource -- req IP :  ${req.ip}
+  header X-Forwarded-For : ${req.header('X-Forwarded-For')}`;
+  res.send(resBody);
 });
 
 module.exports = router;
